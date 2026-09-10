bd864907e86d8519a8c5fbbc7134ea9619d9890e
Arjun262007 <panda2007arjun.s@gmail.com>
2026-09-10 14:25:42 +0530

    Create EmergencyPriority.java

---
+++ src/main/java/com/ambulance/model/EmergencyPriority.java
@@ -0,0 +1,10 @@
+// File: src/main/java/com/ambulance/model/EmergencyPriority.java
+package com.ambulance.model;
+
+public enum EmergencyPriority {
+    CRITICAL(1), HIGH(2), MODERATE(3), NORMAL(4);
+    
+    private final int rank;
+    EmergencyPriority(int rank) { this.rank = rank; }
+    public int getRank() { return rank; }
+}
